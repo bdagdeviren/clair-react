03b103a7d02dba8adc35d6ffd2cd12c6dcddfd2d
Mona Lisa <email@example.com>
2019-10-04 16:28:06 +0300

    burak

--- src/App.js
+++ src/App.js
@@ -1,55 +1,24 @@
 import React from 'react';
-import { forwardRef } from 'react'
 import './App.css';
 import axios from 'axios';
-import MaterialTable from "material-table";
-
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
 
 class Clock extends React.Component {
-    columns = [
-        { field: 'layerid', title: 'Layer\u00a0ID', minWidth: 170 },
-        { field: 'cve', title: 'CVE Dictionary Entry', minWidth: 170 },
-        { field: 'packagename', title: 'Package\u00a0Name', minWidth: 100 },
-        { field: 'packageversion', title: 'Package\u00a0Version', minWidth: 170, align: 'right', format: value => value.toLocaleString(), },
-        { field: 'severity', title: 'Severity', minWidth: 170, align: 'right', format: value => value.toLocaleString(), },
-        { field: 'fixedby', title: 'Fixed By', minWidth: 170, align: 'right', format: value => value.toFixed(2), },
-      ];
 
-      tableIcons = {
-        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-        SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
-        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-      };
-    
+    columns = [
+        {
+            Header: "Vulnerability",
+            columns: [
+                { accessor: 'layerid', Header: 'Layer\u00a0ID', minWidth: 170 },
+                { accessor: 'cve', Header: 'CVE Dictionary Entry', minWidth: 170 },
+                { accessor: 'packagename', Header: 'Package\u00a0Name', minWidth: 100 },
+                { accessor: 'packageversion', Header: 'Package\u00a0Version', minWidth: 170, align: 'right', format: value => value.toLocaleString(), },
+                { accessor: 'severity', Header: 'Severity', minWidth: 170, align: 'right', format: value => value.toLocaleString(), },
+                { accessor: 'fixedby', Header: 'Fixed By', minWidth: 170, align: 'right', format: value => value.toFixed(2), },
+            ]
+        }
+      ]; 
 
     REST_ENDPOINT = 'http://10.150.0.247:8081/';
 
@@ -189,21 +158,16 @@ class Clock extends React.Component {
                         <div id="loading-text">{this.state.loadingText}</div>
                     </div>
                 </div>
-                <div style={{ maxWidth: "100%", display:this.state.tableDisplay }}>
-                    <MaterialTable
-                    style={{height:"100vh"}}
-                    options={{
-                        exportButton: true,
-                        exportFileName: "vulnerability",
-                        rowStyle: {
-                          backgroundColor: '#EEE',
-                        }
-                      }}
-                    icons={this.tableIcons}
-                    columns={this.columns}
+                <div style={{ maxWidth: "100%", display:this.state.tableDisplay,backgroundColor:"white" }}>
+                <ReactTable
                     data={this.createArray(this.state.jsondata)}
-                    title="Demo Title"
-                    />
+                    columns={this.columns}
+                    defaultPageSize={20}
+                    style={{
+                        height: "calc(100vh - 2px)"
+                    }}
+                    className="-striped -highlight"
+                />
                 </div>
             </div>
         )
